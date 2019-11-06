d890cecbdf703703015228177efdf71bec34f092
DannyLamarti <lamarti.danny@gmail.com>
2019-11-06 14:39:35 +0100

    remove constraints from readonlyProperyOf

--- library/src/main/kotlin/io/lamart/deps/utils.kt
+++ library/src/main/kotlin/io/lamart/deps/utils.kt
@@ -3,7 +3,7 @@ package io.lamart.deps
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
-inline fun <R, reified T : Any> readOnlyPropertyOf(crossinline get: () -> T) =
+fun <R, T> readOnlyPropertyOf(get: () -> T) =
     object : ReadOnlyProperty<R, T> {
         override fun getValue(thisRef: R, property: KProperty<*>): T = get()
     }
